51b69e54602b9fe864effcd7fd0c7debeb84a556
Andriy112 <86891549+Andriy112@users.noreply.github.com>
2022-11-29 23:31:20 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace Web_Server
 
             webServer.AddPage(null, "<h1>text</h1>" + Environment.NewLine + "<p>hello</p>");
             webServer.AddPage("index", "<h1>(website)</h1>");
-            webServer.AddPage("about/van", "<p>iVan Temnoholm</p>");
+            webServer.AddPage("about/van", "<p>text here</p>");
             webServer.AddPage("redirectpage", "<a href=https://logodix.com/aspnet>Visit</a>");
             webServer.AddPage("video", "<!DOCTYPE html> <html> <body> <iframe width=\"640\"height=\"480\" src=\"https://www.youtube.com/embed/tgbNymZ7vqY\"> </iframe> </body> </html>");
             WebServer.WebApplicationRunner.Run();
